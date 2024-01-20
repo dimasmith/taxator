6f45f957c4be64af76c3e6830b5c2119567b03c5
Dmytro Kovalchuk <dimasmith@gmail.com>
2024-01-20 23:13:44 +0200

    feat: record and calculate multiple incomes

--- taxator-ui/src/main.rs
+++ taxator-ui/src/main.rs
@@ -1,19 +1,27 @@
-use eframe::egui::{CentralPanel, Context, TextEdit, Vec2};
-use eframe::{run_native, App, CreationContext, Frame, NativeOptions};
+use eframe::{App, CreationContext, Frame, NativeOptions, run_native};
+use eframe::egui::{CentralPanel, Context, Grid, TextEdit, Vec2};
 
 use taxator_calculator::money::{Currency, Money};
 
 struct TaxHelperApp {
-    income: Option<Money>,
+    incomes: Vec<Money>,
     tax_amount: Option<Money>,
+    edited_amount: Money,
 }
 
 impl TaxHelperApp {
     pub fn new(_cc: &CreationContext) -> Self {
         Self {
-            income: None,
+            incomes: vec![],
             tax_amount: None,
+            edited_amount: Money::new(0., Currency::UAH),
         }
+    }
+
+    fn calculate_total(&mut self) -> Money {
+        let total = self.incomes.iter()
+            .fold(Money::new(0., Currency::UAH), |x, y| x.add(y).unwrap());
+        total
     }
 }
 
@@ -23,24 +31,43 @@ impl App for TaxHelperApp {
             ui.vertical_centered_justified(|ui| {
                 ui.heading("Tax Helper Application");
 
-                let mut amount_str = match self.income {
-                    Some(income) => format!("{:.2}", income.amount()),
-                    None => format!("{:.2}", 0.),
-                };
+                Grid::new("incomes_grid").show(ui, |ui| {
+                    for (idx, income) in self.incomes.iter().enumerate() {
+                        ui.label(format!("{}", idx + 1));
+                        ui.label(format!("{:.2}", income.amount()));
+                        ui.end_row();
+                    }
+                    ui.label("Total");
+                    let total = self.calculate_total();
+                    ui.label(format!("{:.2}", total.amount()));
+                    ui.end_row();
+
+                });
+
+                let mut amount_str = format!("{:.2}", self.edited_amount.amount());
                 let amount_edit =
                     ui.add(TextEdit::singleline(&mut amount_str).hint_text("Tax amount"));
                 if amount_edit.changed() {
                     if let Ok(new_income_amount) = amount_str.parse::<f32>() {
                         let new_income = Money::new(new_income_amount, Currency::UAH);
-                        self.income.replace(new_income);
+                        self.edited_amount = new_income;
                     }
                 }
 
-                let calculate_button = ui.button("Calculate");
-                if let Some(income) = self.income {
+                if self.edited_amount.amount() > 0. {
+                    let record_amount_button = ui.button("Record amount");
+                    if record_amount_button.clicked() {
+                        self.incomes.push(self.edited_amount);
+                        self.edited_amount = Money::new(0., Currency::UAH);
+                    }
+                }
+
+                if !self.incomes.is_empty() {
+                    let calculate_button = ui.button("Calculate");
                     if calculate_button.clicked() {
-                        let tax = income.percent(5.0);
-                        self.tax_amount.replace(tax);
+                        let total = self.calculate_total();
+                        let tax = total.percent(5.0);
+                        self.tax_amount = Some(tax);
                     }
                 }
 
